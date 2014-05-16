6069f6ac479400d4abbb5d7dde3ddfd5291922f1
Weslley Araujo <wparaujo@sp.r7.com>
2014-05-16 19:27:36 -0300

    add date :)

---
+++ spec/dateSpec.js
@@ -0,0 +1,20 @@
+/**
+ * Specs for Date class.
+ *
+ * @class Date
+ */
+describe('Date', function () {
+
+	beforeEach(function () {
+		this.date = new Date();
+	});
+
+	/**
+	 * Specs for addhours method
+	 *
+	 * @method addhours
+	 */
+	describe('addhours', function () {
+	});
+
+});
