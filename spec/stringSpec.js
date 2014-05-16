3e6a6876a9f103da805bf096dd29d51cdcb611c2
Weslley Araujo <wparaujo@sp.r7.com>
2014-05-16 18:25:18 -0300

    Add truncate

--- spec/stringSpec.js
+++ spec/stringSpec.js
@@ -1,5 +1,15 @@
+/**
+ * Specs for String class.
+ *
+ * @class String
+ */
 describe('String', function () {
 
+	/**
+	 * Specs for normalize method
+	 *
+	 * @method normalize
+	 */
 	describe('normalize', function () {
 		it('remove accents from string', function () {
 			expect('Whát dões fóx sãys'.normalize()).toBe('What does fox says');
@@ -10,9 +20,29 @@ describe('String', function () {
 		});
 	});
 
+	/**
+	 * Specs for toSlug method
+	 *
+	 * @method toSlug
+	 */
 	describe('toSlug', function () {
 		it('converts a string into a slug', function () {
 			expect('What does the fox says'.toSlug()).toBe('what-does-the-fox-says');
+		});
+	});
+
+	/**
+	 * Specs for truncate method
+	 *
+	 * @method truncate
+	 */
+	describe('truncate', function () {
+		it('truncates a string with the terminator ...', function () {
+			expect('What does the fox'.truncate(16)).toBe('What does the...');
+		});
+
+		it('keep the string equal', function () {
+			expect('What does the fox'.truncate(20)).toBe('What does the fox');
 		});
 	});
 
