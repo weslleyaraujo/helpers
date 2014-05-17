38ebc0e3c99caf79281766b6832a8f2d86766497
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-05-16 23:15:38 -0300

    add spec for date

--- spec/dateSpec.js
+++ spec/dateSpec.js
@@ -5,8 +5,8 @@
  */
 describe('Date', function () {
 
-	beforeEach(function () {
-		this.date = new Date();
+	beforeEach(function() {
+		this.date = new Date('00');
 	});
 
 	/**
@@ -15,6 +15,42 @@ describe('Date', function () {
 	 * @method addhours
 	 */
 	describe('addhours', function () {
+		it('add one hour to the date', function () {
+			expect(this.date.addHours(1).getHours()).toBe(23);
+		});
+	});
+
+	/**
+	 * Specs for removeHours
+	 *
+	 * @method removeHours
+	 */
+	describe('removeHours', function () {
+		it('removes an hour from the date', function () {
+			expect(this.date.removeHours(1).getHours()).toBe(21);
+		});
+	});
+
+	/**
+	 * Specs for addMinutes
+	 *
+	 * @method addMinutes
+	 */
+	describe('addMinutes', function () {
+		it('add a minute to the date', function () {
+			expect(this.date.addMinutes(1).getMinutes()).toBe(1);
+		});
+	});
+
+	/**
+	 * Specs for removeMinutes
+	 *
+	 * @method removeMinutes
+	 */
+	describe('removeMinutes', function () {
+		it('removes a minute from date', function () {
+			expect(this.date.addMinutes(1).removeMinutes(1).getMinutes()).toBe(0);
+		});
 	});
 
 });
