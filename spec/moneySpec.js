409d6f2fb0c6f2916d864a964cae6210c872f1d9
Weslley Araujo <wparaujo@sp.r7.com>
2014-05-20 18:15:55 -0300

    bigodon

--- spec/moneySpec.js
+++ spec/moneySpec.js
@@ -15,9 +15,28 @@ describe('Money', function () {
 	 * @method format
 	 */
 	describe('format', function () {
-		it('tracks the format', function () {
+		it('make sure that the date has format', function () {
 			this.money = new Money();
 			expect(this.money.format).toHaveBeenCalled();
+		});
+
+		it('creates a string with money format', function () {
+			this.money = new Money();
+			expect(this.money.toString()).toBe('R$100,00');
+		});
+	});
+
+	/**
+	 * Specs for separateThousands method
+	 *
+	 * @method separateThousands
+	 */
+	describe('separateThousands', function () {
+		it('separate a value with dots', function () {
+			this.money = new Money();
+			this.money.string_amount = '100';
+			console.log(this.money.separateThousands());
+			// expect(this.money.format).toHaveBeenCalled();
 		});
 	});
 
